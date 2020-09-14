eeef18077a7b951fff23707859f8717822db9153
cdemarti <codythomasdemartin@gmail.com>
2020-09-14 15:10:04 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+class EyesOnMe extends Component {
+
+    constructor() {
+        super()
+    }
+
+    noFocus = (event) => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    focus = (event) => {
+        console.log('Good!')
+    }
+
+    render() {
+        return (
+            <button 
+                onBlur = {this.noFocus}
+                onFocus= {this.focus} >
+            </button>
+        )
+    }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
-// Code Keypad Component Here
+import React, { Component } from 'react'
+
+class Keypad extends Component {
+
+    constructor() {
+        super()
+    }
+
+    handleKey = (event) => {
+        console.log("Entering password...")
+    }
+
+    render() {
+        return (
+            <input 
+                type="password"
+                onKeyUp = {this.handleKey} >
+            </input>
+        )
+    }
+}
+
+export default Keypad
