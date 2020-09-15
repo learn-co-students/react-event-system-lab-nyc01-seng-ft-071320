0627ae5695077edc9c86267a3aac978c14e1ed4c
Ryan Flynn <ryanflynn44@gmail.com>
2020-09-14 21:27:52 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component{
+
+    onFocusHandler = () => {
+        console.log('Good!')
+    }
+    blurHandler = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render(){
+        return <button onFocus={this.onFocusHandler} onBlur={this.blurHandler} ></button>
+    }
+
+
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from 'react'
+
+class KeyPad extends React.Component {
+
+    keyUpHandler = () => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return <input onKeyUp={this.keyUpHandler} type="password"></input>
+    }
+
+}
+
+export default KeyPad
