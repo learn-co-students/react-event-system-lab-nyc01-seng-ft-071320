0f9aea258bbfb14bdf98dc01121bd34b659da51e
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-09-15 21:11:21 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component{
+
+    focusYandler = () => {
+        console.log('Good!')
+    }
+
+    blurYandler = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+
+    render() {
+        return (<button onBlur={this.blurYandler} onFocus={this.focusYandler}></button>)
+    }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from 'react'
+
+class Keypad extends React.Component{
+
+    eventHandeler = (e) => {
+      console.log('Entering password...')
+    }
+
+
+    render() {
+        return (<input onKeyUp={this.eventHandeler} type="password" />)
+    }
+}
+
+export default Keypad
